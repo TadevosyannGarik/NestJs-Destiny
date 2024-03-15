74a555b91f58f5d76cecef8f4cb0662491803510
TadevosyannGarik <tadevosyan12@gmail.com>
2024-03-15 18:18:23 +0400

    Marketing Skeleton

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,11 +1,10 @@
 import { Button } from "@/components/ui/button";
 
+
 export default function Home() {
     return (
         <div>
-            <Button>
-                Click Me
-            </Button>
+            This is marketing page
         </div>
     );
-}
+};
