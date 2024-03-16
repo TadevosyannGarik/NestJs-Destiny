2b64e5a792959e26d402ee35d88c160c5a5ad0cd
TadevosyannGarik <tadevosyan12@gmail.com>
2024-03-16 19:20:26 +0400

    User Progress

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "@/components/ui/sonner";
 
 
 const font = Nunito({ subsets: ["latin"] });
@@ -16,6 +17,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
         <ClerkProvider>
             <html lang="en">
                 <body className={font.className}>
+                    <Toaster />
                     {children}
                 </body>
             </html>
