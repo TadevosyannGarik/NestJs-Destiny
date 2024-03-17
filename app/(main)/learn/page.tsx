9b7c24142c2165236498834886af12666c726130
TadevosyannGarik <tadevosyan12@gmail.com>
2024-03-17 16:55:27 +0400

    Course Progress

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -2,19 +2,26 @@ import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickyWrapper } from "@/components/sticky-wrapper";
 import { Header } from "./header";
 import { UserProgress } from "@/components/user-progress";
-import { getUnits, getUserProgress } from "@/db/queries";
+import { getCourseProgress, getLessonPercentage, getUnits, getUserProgress } from "@/db/queries";
 import { redirect } from "next/navigation";
 import { Unit } from "./unit";
+import { lessons, units as unitsSchema } from "@/db/schema";
 
 
 const LearnPage = async () => {
     const userProgressData = getUserProgress();
     const unitsData = getUnits();
+    const courseProgressData = getCourseProgress();
+    const lessonPresentageData = getLessonPercentage(); 
 
-    const [userProgress, units] = await Promise.all([userProgressData, unitsData]);
+    const [userProgress, units, courseProgress, lessonPresentage] = await Promise.all([userProgressData, unitsData, courseProgressData, lessonPresentageData]);
 
     if (!userProgress || !userProgress.activeCourse) {
-        redirect("/courses")
+        redirect("/courses");
+    }
+
+    if (!courseProgress) {
+        redirect("/courses");
     }
 
     return (
@@ -37,8 +44,10 @@ const LearnPage = async () => {
                             description={unit.description}
                             title={unit.title}
                             lessons={unit.lessons}
-                            activeLesson={undefined}
-                            activeLessonsPercentage={0}
+                            activeLesson={courseProgress.activeLesson as typeof lessons.$inferSelect & {
+                                unit: typeof unitsSchema.$inferSelect;
+                              } | undefined}
+                            activeLessonsPercentage={lessonPresentage}
                         />
                     </div>
                 ))}
