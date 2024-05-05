bf962ee4e2d23459eac70cd89c2ec3feea7d9816
TadevosyannGarik <tadevosyan12@gmail.com>
2024-05-05 17:20:08 +0400

    Details

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -6,6 +6,8 @@ import { getCourseProgress, getLessonPercentage, getUnits, getUserProgress, getU
 import { redirect } from "next/navigation";
 import { Unit } from "./unit";
 import { lessons, units as unitsSchema } from "@/db/schema";
+import { Promo } from "@/components/promo";
+import { Quests } from "@/components/quests";
 
 
 const LearnPage = async () => {
@@ -25,6 +27,8 @@ const LearnPage = async () => {
         redirect("/courses");
     }
 
+    const isPro = !!userSubscription?.isActive;
+
     return (
         <div className="flex flex-row-reverse gap-[48px] px-6">
             <StickyWrapper>
@@ -34,6 +38,10 @@ const LearnPage = async () => {
                     points={userProgress.points}
                     hasActiveSubscription={!!userSubscription?.isActive}
                 />
+                {!isPro && (
+                    <Promo />
+                )}
+                <Quests points={userProgress.points}/>
             </StickyWrapper>
             <FeedWrapper>
                 <Header title={userProgress.activeCourse.title}/>
